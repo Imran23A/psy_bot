57dbc396eb2d68311d40455f71f66101353ee4d3
Imran23A <117780822+Imran23A@users.noreply.github.com>
2023-07-25 15:08:38 +0000

    Working without menu bot.py

---
+++ bot1.py
@@ -0,0 +1,216 @@
+import logging
+import csv
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
+from telegram.ext import Updater, CommandHandler, CallbackContext, ConversationHandler, CallbackQueryHandler, MessageHandler, Filters
+import datetime
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# State definitions for top-level conversation
+SELECTING_QUESTIONS = 1
+TEST_IN_PROGRESS = 2
+
+# Constants for this example
+QUESTIONS_FILE = "questions.tsv"
+RESULTS_FILE = "results.csv"
+QUESTIONS_PER_TEST = 21
+
+# A dictionary containing the questions and options read from the questions.tsv file
+questions = {}
+
+
+def read_questions_from_file():
+    try:
+        with open(QUESTIONS_FILE, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            for line in lines:
+                data = line.strip().split('\t')
+                if len(data) == 5:
+                    question = data[0]
+                    options = data[1:]
+                    questions[question] = options
+    except FileNotFoundError:
+        raise Exception(f"Failed to read questions from the file '{QUESTIONS_FILE}'. Make sure it exists.")
+    return questions
+
+
+def main_menu():
+    # Create the main menu keyboard
+    menu_keyboard = [
+        [KeyboardButton("Start Test")]
+    ]
+    return ReplyKeyboardMarkup(menu_keyboard, one_time_keyboard=True, resize_keyboard=True)
+
+
+def start(update: Update, _: CallbackContext) -> int:
+    user_id = update.effective_user.id
+    user_data = _.user_data.setdefault(user_id, {})
+
+    # Show the main menu with "Start Test" button
+    update.message.reply_text("Hello! I'm your depression test bot. How can I assist you?", reply_markup=main_menu())
+
+    return SELECTING_QUESTIONS
+
+
+def start_exam(update: Update, _: CallbackContext):
+    user_id = update.effective_user.id
+    user_data = _.user_data.setdefault(user_id, {})
+
+    # Check if the user is already in the middle of a test
+    if 'current_question' in user_data:
+        # Clear ongoing test data to start a new one
+        user_data.clear()
+
+    # Start a new test
+    read_questions_from_file()
+    user_data['current_question'] = 1  # Start from the first question
+    user_data['answers'] = {}  # Initialize or reset answers dictionary
+
+    # If there was a previous message, edit it with the new test
+    previous_message_id = user_data.get('previous_message')
+    if previous_message_id:
+        update.callback_query.edit_message_text("Test started! Please answer the following questions:")
+
+    next_question(update, _)
+
+    return TEST_IN_PROGRESS
+
+
+def next_question(update: Update, _: CallbackContext):
+    user_id = update.effective_user.id
+    user_data = _.user_data.get(user_id)
+    if not user_data:
+        return
+
+    current_question_number = user_data.get('current_question')
+    if not current_question_number or current_question_number > QUESTIONS_PER_TEST:
+        # Test is completed, show results as a "fake" last question
+        show_results(update, _)
+        return SELECTING_QUESTIONS
+
+    question = list(questions.keys())[current_question_number - 1]
+    options = questions[question]
+
+    keyboard = []
+    for idx, option in enumerate(options):
+        keyboard.append([InlineKeyboardButton(option, callback_data=str(idx))])
+
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # If there's a previous message, edit it, otherwise send a new one
+    previous_message_id = user_data.get('previous_message')
+    if previous_message_id:
+        update.callback_query.edit_message_text(
+            text=question,
+            reply_markup=reply_markup
+        )
+    else:
+        message = update.effective_message.reply_text(question, reply_markup=reply_markup)
+        user_data['previous_message'] = message.message_id
+
+    user_data['current_question'] += 1
+
+
+def handle_answer(update: Update, _: CallbackContext) -> int:
+    query = update.callback_query
+    query.answer()
+
+    user_id = update.effective_user.id
+    user_data = _.user_data.get(user_id)
+    if not user_data:
+        return ConversationHandler.END
+
+    question_number = user_data.get('current_question')
+    if not question_number or question_number > QUESTIONS_PER_TEST:
+        # Should not happen, but just in case
+        show_results(update, _)
+        return SELECTING_QUESTIONS
+
+    chosen_option = int(query.data)
+    user_data['answers'][question_number] = chosen_option
+    user_data['current_question'] += 1
+
+    return next_question(update, _)
+
+
+def show_results(update: Update, _: CallbackContext):
+    user_id = update.effective_user.id
+    user_data = _.user_data.get(user_id)
+    if not user_data:
+        return
+
+    answers = user_data.get('answers', {})
+    total_score = sum(answers.values())
+
+    result = ""
+    if total_score <= 9:
+        result = "You have no depressive symptoms."
+    elif 10 <= total_score <= 15:
+        result = "You may have mild depression (subdepression)."
+    elif 16 <= total_score <= 19:
+        result = "You have moderate depression."
+    elif 20 <= total_score <= 29:
+        result = "You have severe depression (moderate severity)."
+    else:
+        result = "You have severe depression."
+
+    # Save results in CSV file
+    timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    with open(RESULTS_FILE, 'a', newline='') as csvfile:
+        fieldnames = ['user_id', 'timestamp', 'result']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow({'user_id': user_id, 'timestamp': timestamp, 'result': total_score})
+
+    # If there was a previous message, edit it with the results, otherwise send a new message
+    previous_message_id = user_data.get('previous_message')
+    if previous_message_id:
+        update.callback_query.edit_message_text(
+            text=f"Test completed! Your results: {result}",
+            reply_markup=None  # Remove the inline keyboard
+        )
+    else:
+        update.effective_message.reply_text(f"Test completed! Your results: {result}")
+
+    # Clear user data after showing results
+    user_data.clear()
+
+    return SELECTING_QUESTIONS
+
+
+def main():
+    # Create the Updater and pass it your bot's token
+    with open("token.txt", "r") as file:
+        token = file.read().strip()
+
+    updater = Updater(token, use_context=True)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add conversation handler with the state SELECTING_QUESTIONS
+    conversation_handler = ConversationHandler(
+        entry_points=[MessageHandler(Filters.text("Start Test"), start_exam)],
+        states={
+            SELECTING_QUESTIONS: [
+                CallbackQueryHandler(handle_answer, pattern='^[0-3]$'),
+            ],
+            TEST_IN_PROGRESS: [
+                CallbackQueryHandler(handle_answer, pattern='^[0-3]$'),
+            ],
+        },
+        fallbacks=[]
+    )
+
+    dp.add_handler(conversation_handler)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until the user presses Ctrl-C or the process receives SIGINT, SIGTERM, or SIGABRT
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
